da4c2c2ce9d858cfbf27285c633fb2fdbca5c884
HamiciRyadh <hamiciryadh@gmail.com>
2018-03-07 22:14:30 +0100

    first commit

---
+++ Web Service/src/com/lfbservices/usthb/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.lfbservices.usthb;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashMap;
+import java.util.Map;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+	
+
+}
